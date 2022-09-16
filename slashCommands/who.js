ccb526b5038914a8044a0ccd9152b014393d6aef
Knightplayzz <73634415+Knightplayzz@users.noreply.github.com>
2022-09-16 13:58:28 +0200

    Update who.js

--- slashCommands/who.js
+++ slashCommands/who.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require("discord.js");
-const fetch = require('node-fetch')
 const discord = require("discord.js")
 
 module.exports = {
@@ -23,43 +22,18 @@ module.exports = {
         try {
             var type = interaction.options.get('type').value
             var Offensive = ["Ash", "Bangalore", "Fuse", "Horizon", "Mad Maggie", "Mirage", "Octane", "Revenant", "Wraith"]
-            var Offensive_link = [
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Ash.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bangalore.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Fuse.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Horizon.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mad%20Maggie.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mirage.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Octane.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Revenant.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wraith.png',
-
-            ]
+            var Offensive_link = ['https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Ash.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bangalore.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Fuse.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Horizon.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mad%20Maggie.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mirage.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Octane.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Revenant.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wraith.png']
             var Defensive = ["Caustic", "Gibraltar", "Newcastle", "Wattson"]
-            var Defensive_link = [
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Caustic.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Gibraltar.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Newcastle.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wattson.png'
-            ]
+            var Defensive_link = ['https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Caustic.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Gibraltar.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Newcastle.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wattson.png']
             var Support = ["Lifeline", "Loba"]
-            var Support_link = [
-                "https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Lifeline.png",
-                "https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Loba.png"
-            ]
+            var Support_link = ["https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Lifeline.png", "https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Loba.png"]
             var Recon = ["Bloodhound", "Crypto", "Pathfinder", "Seer", "Valkyrie", "Vantage"]
-            var Recon_link = [
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bloodhound.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Crypto.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Pathfinder.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Seer.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Valkyrie.png',
-                'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Vantage.png'
-            ]
+            var Recon_link = ['https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bloodhound.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Crypto.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Pathfinder.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Seer.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Valkyrie.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Vantage.png']
 
             if (type === "Offensive") {
-                let y = Offensive.length - 1
-                let x = Math.floor(Math.random() * (y - 0))
+                console.log(Offensive_link[x])
+                let y = Offensive.length
+                let x = Math.floor(Math.random() * (y))
                 let botEmbed = new discord.MessageEmbed()
                     .setDescription(`Play **${Offensive[x]}** this round!`)
                     .setImage(Offensive_link[x])
@@ -70,8 +44,8 @@ module.exports = {
                 return interaction.reply({ embeds: [botEmbed], ephemeral: true })
             }
             if (type === "Defensive") {
-                let y = Defensive.length - 1
-                let x = Math.floor(Math.random() * (y - 0))
+                let y = Defensive.length
+                let x = Math.floor(Math.random() * (y))
                 let botEmbed = new discord.MessageEmbed()
                     .setDescription(`Play **${Defensive[x]}** this round!`)
                     .setImage(Defensive_link[x])
@@ -81,18 +55,19 @@ module.exports = {
                 return interaction.reply({ embeds: [botEmbed], ephemeral: true })
             }
             if (type === "Support") {
-                let y = Support.length - 1
-                let x = Math.floor(Math.random() * (y - 0))
+                let y = Support.length
+                let x = Math.floor(Math.random() * (y))
                 let botEmbed = new discord.MessageEmbed()
                     .setDescription(`Play **${Support[x]}** this round!`)
+                    .setImage(Support_link[x])
                     .setFooter(`${client.user.username} ❤️`)
                     .setTimestamp()
 
                 return interaction.reply({ embeds: [botEmbed], ephemeral: true })
             }
             if (type === "Recon") {
-                let y = Recon.length - 1
-                let x = Math.floor(Math.random() * (y - 0))
+                let y = Recon.length
+                let x = Math.floor(Math.random() * (y))
                 let botEmbed = new discord.MessageEmbed()
                     .setDescription(`Play **${Recon[x]}** this round!`)
                     .setImage(Recon_link[x])
@@ -109,33 +84,10 @@ module.exports = {
 
 
         var legends = ['Ash', 'Bangalore', 'Bloodhound', 'Caustic', 'Crypto', 'Fuse', 'Gibraltar', 'Horizon', 'Lifeline', 'Loba', 'Mad Maggie', 'Mirage', 'Newcastle', 'Octane', 'Pathfinder', 'Rampart', 'Revenant', 'Seer', 'Valkyrie', 'Vantage', 'Wraith', 'Wattson']
-        var legendLink = [
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Ash.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bangalore.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bloodhound.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Caustic.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Crypto.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Fuse.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Gibraltar.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Horizon.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Lifeline.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Loba.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mad%20Maggie.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mirage.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Newcastle.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Octane.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Pathfinder.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Rampart.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Revenant.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Seer.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Valkyrie.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Vantage.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wraith.png',
-            'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wattson.png'
-        ]
-        var y = legends.length - 1
+        var legendLink = ['https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Ash.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bangalore.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bloodhound.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Caustic.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Crypto.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Fuse.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Gibraltar.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Horizon.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Lifeline.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Loba.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mad%20Maggie.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mirage.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Newcastle.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Octane.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Pathfinder.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Rampart.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Revenant.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Seer.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Valkyrie.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Vantage.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wraith.png', 'https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wattson.png']
+        var y = legends.length
 
-        var x = Math.floor(Math.random() * (y - 0))
+        var x = Math.floor(Math.random() * (y))
 
         var botEmbed = new discord.MessageEmbed()
             .setDescription(`Play **${legends[x]}** this round!`)
@@ -144,29 +96,5 @@ module.exports = {
             .setTimestamp()
 
         interaction.reply({ embeds: [botEmbed], ephemeral: true })
-
-
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Ash.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bangalore.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Bloodhound.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Caustic.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Crypto.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Fuse.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Gibraltar.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Horizon.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Lifeline.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Loba.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mad%20Maggie.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Mirage.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Newcastle.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Octane.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Pathfinder.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Rampart.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Revenant.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Seer.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Valkyrie.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wattson.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Wraith.png
-        // https://cdn.jumpmaster.xyz/Bot/Legends/Banners/Vantage.png
     }
 }
